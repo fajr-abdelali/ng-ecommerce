39da8181804b427b7bc8072292c4451275b6031e
abdelali fajr <fajr.abdelali@outlook.com>
2023-10-15 17:00:52 +0200

    get products from store and show them in product list

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import * as fromProduct from 'src/app/store/product/index';
+import IProduct from 'src/app/interfaces/product.interface';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
+  products$?: Observable<IProduct[]>;
+  isLoading$?: Observable<boolean>;
+  constructor(private readonly store: Store) { }
+
+  ngOnInit(): void {
+    this.initDispatch();
+    this.initSubscriptions()
+  }
+
+  private initDispatch(): void {
+    this.store.dispatch(fromProduct.getProducts())
+  }
+
+  private initSubscriptions() {
+    this.products$ = this.store.pipe(select(fromProduct.selectProductList)) as Observable<IProduct[]>;
+    this.isLoading$ = this.store.pipe(select(fromProduct.selectProductIsLoading)) as Observable<boolean>;
+  }
 
 }
