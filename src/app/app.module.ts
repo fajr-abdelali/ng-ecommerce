bbc745e89e882e6b3e79319277f2146ee9044b28
abdelali fajr <fajr.abdelali@outlook.com>
2023-10-17 16:31:46 +0200

    add cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { IconsModule } from './shared/module/icons/icons.module';
-
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SliderComponent } from './shared/slider/slider.component';
 import { NewArrivalComponent } from './pages/home/new-arrival/new-arrival.component';
@@ -15,12 +11,12 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 import { ProductComponent } from './shared/product/product.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FilterComponent } from './components/product-list/filter/filter.component';
 import { DropdownComponent } from './shared/inputs/dropdown/dropdown.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { WishButtonComponent } from './shared/wish-button/wish-button.component';
 import { SuggestionsComponent } from './components/product-detail/suggestions/suggestions.component';
+import { SharedModule } from './shared/shared.module'
 
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
@@ -31,8 +27,6 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
     HomeComponent,
     SliderComponent,
     NewArrivalComponent,
@@ -45,17 +39,17 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
     HeaderComponent,
     WishButtonComponent,
     SuggestionsComponent,
+    // CartComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule,
-    IconsModule,
     StoreModule.forRoot(),
     EffectsModule.forRoot(),
     StoreDevtoolsModule.instrument({}),
     ProductStoreModule,
-    CartStoreModule
+    CartStoreModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { IconsModule } from './module/icons/icons.module';
+
+import { NavbarComponent } from './navbar/navbar.component';
+import { CartComponent } from './navbar/cart/cart.component'
+import { FooterComponent } from './footer/footer.component';
+import { CartItemComponent } from './navbar/cart/cart-item/cart-item.component';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    CartComponent,
+    FooterComponent,
+    CartItemComponent,
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    IconsModule,
+  ],
+  exports:[
+    NavbarComponent,
+    FooterComponent,
+    FontAwesomeModule,
+  ]
+})
+export class SharedModule { }
