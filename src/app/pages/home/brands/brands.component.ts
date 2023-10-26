41c3812a01d34fac40698f873902bbaa81df2447
abdelali fajr <fajr.abdelali@outlook.com>
2023-10-26 13:03:40 +0200

    brand component

---
+++ src/app/pages/home/brands/brands.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-brands',
+  templateUrl: './brands.component.html',
+  styleUrls: ['./brands.component.scss']
+})
+export class BrandsComponent {
+  items = [
+    { id: 1, title: 'Item 1' },
+    { id: 2, title: 'Item 2' },
+    { id: 3, title: 'Item 3' },
+    // Add more items as needed
+  ];
+
+}
